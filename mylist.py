5b4cbd43c6fa2b571710197b99d9ebdde6c2e528
UC-Nelson <uwakwenelson01@gmail.com>
2025-08-29 01:35:03 +0100

    Week 2 python assignment

---
+++ mylist.py
@@ -0,0 +1,25 @@
+# Question  1: Create an empty list
+my_list = []
+
+# Question 2: Append elements 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Question 3: Insert the value 15 at the second position
+my_list.insert(1, 15)
+
+# Question 4: Extend the list with another list [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+
+# Question 5: Remove the last element from the list
+my_list.pop()
+
+# Question 6: Sort the list in ascending order
+my_list.sort()
+
+# Question 7: Find and print the index of the value 30
+index_of_30 = my_list.index(30)
+print("The index of 30 is:", index_of_30)
